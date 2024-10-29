112136783914f7cadc0f1fdafead32083736bec1
Marek Skopal <skopal.marek@gmail.com>
2024-10-29 15:49:33 +0100

    Refactored tsconfig to new format

--- ext_tables.php
+++ ext_tables.php
@@ -5,6 +5,3 @@ declare(strict_types=1);
 defined('TYPO3') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ms_core', 'Configuration/TypoScript', 'MS - Core');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ms_core/Configuration/TSConfig/rte.txt">');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ms_core/Configuration/TSConfig/user.txt">');
