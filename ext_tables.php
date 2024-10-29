819324d37d0005c9a6d249c2480cbaa9ab8ca2d1
Marek Skopal <skopal.marek@gmail.com>
2024-10-29 20:33:14 +0100

    Move typoscript to Sets

--- ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-defined('TYPO3') or die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ms_core', 'Configuration/TypoScript', 'MS - Core');
