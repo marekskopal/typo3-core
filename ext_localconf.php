00510121357882fc7f3eb13053f6072da64db493
Marek Skopal <skopal.marek@gmail.com>
2025-11-14 00:38:40 +0100

    Use ::class

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,15 +6,15 @@ defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ms_core'] = 'EXT:ms_core/Configuration/RTE/Default.yaml';
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\DataProcessing\\LanguageMenuProcessor'] = [
-    'className' => 'Skopal\\MsCore\\DataProcessing\\LanguageMenuProcessor'
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Frontend\DataProcessing\LanguageMenuProcessor::class] = [
+    'className' => Skopal\MsCore\DataProcessing\LanguageMenuProcessor::class
 ];
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['GeorgRinger\\News\\Controller\\NewsController'] = [
-    'className' => 'Skopal\\MsCore\\Controller\\NewsController'
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][GeorgRinger\News\Controller\NewsController::class] = [
+    'className' => Skopal\MsCore\Controller\NewsController::class
 ];
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Tpwd\\KeSearch\\Plugins\\ResultlistPlugin'] = [
-    'className' => 'Skopal\\MsCore\\Plugins\\ResultlistPlugin'
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][Tpwd\KeSearch\Plugins\ResultlistPlugin::class] = [
+    'className' => Skopal\MsCore\Plugins\ResultlistPlugin::class
 ];
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\RecordList\\DatabaseRecordList'] = [
-    'className' => 'Skopal\\MsCore\\RecordList\\DatabaseRecordList'
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\RecordList\DatabaseRecordList::class] = [
+    'className' => Skopal\MsCore\RecordList\DatabaseRecordList::class
 ];
