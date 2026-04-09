352b06759ba4c6de65cc599829f28b67c300b614
Marek Skopal <skopal.marek@gmail.com>
2026-04-09 22:43:19 +0200

    Revert NewsController

--- Classes/Controller/NewsController.php
+++ Classes/Controller/NewsController.php
@@ -5,27 +5,15 @@ declare(strict_types=1);
 namespace Skopal\MsCore\Controller;
 
 use GeorgRinger\News\Domain\Model\News;
-use GeorgRinger\News\Domain\Repository\CategoryRepository;
-use GeorgRinger\News\Domain\Repository\NewsRepository;
-use GeorgRinger\News\Domain\Repository\TagRepository;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\LanguageAspect;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Frontend\Controller\ErrorController;
 use TYPO3\CMS\Frontend\Page\PageAccessFailureReasons;
 
 class NewsController extends \GeorgRinger\News\Controller\NewsController
 {
-    public function __construct(
-        NewsRepository $newsRepository,
-        CategoryRepository $categoryRepository,
-        TagRepository $tagRepository,
-        private readonly Context $context,
-        private readonly ErrorController $errorController,
-    ) {
-        parent::__construct($newsRepository, $categoryRepository, $tagRepository);
-    }
-
     /**
      * Single view of a news record
      *
@@ -35,7 +23,7 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController
     public function detailAction(?News $news = null, $currentPage = 1): ResponseInterface
     {
         /** @var LanguageAspect $languageAspect */
-        $languageAspect = $this->context->getAspect('language');
+        $languageAspect = GeneralUtility::makeInstance(Context::class)->getAspect('language');
         $sysLanguageUid = $languageAspect->getId();
 
         if ($news !== null && $news->getSysLanguageUid() !== $sysLanguageUid) {
@@ -56,7 +44,8 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController
             )->execute()->getFirst();
 
             if ($news === null) {
-                return $this->errorController->pageNotFoundAction(
+                $errorController = GeneralUtility::makeInstance(ErrorController::class);
+                return $errorController->pageNotFoundAction(
                     $this->request,
                     'The requested page does not exist',
                     ['code' => PageAccessFailureReasons::PAGE_NOT_FOUND]
